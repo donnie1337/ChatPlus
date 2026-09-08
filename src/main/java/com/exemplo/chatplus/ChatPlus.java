e7aeb4dcc404264398c57c24b2d52624b86f5e87
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-08 02:14:10 -0300

    centralizar mensagens de entrada e saida no UtilidadesPlus

--- src/main/java/com/exemplo/chatplus/ChatPlus.java
+++ src/main/java/com/exemplo/chatplus/ChatPlus.java
@@ -8,7 +8,6 @@ import com.exemplo.chatplus.command.LocalChatCommand;
 import com.exemplo.chatplus.command.StaffChatCommand;
 import com.exemplo.chatplus.config.ConfigManager;
 import com.exemplo.chatplus.listener.ChatListener;
-import com.exemplo.chatplus.listener.EntradaSaidaListener;
 import com.exemplo.chatplus.listener.UnknownCommandListener;
 import com.exemplo.chatplus.service.ChatColorService;
 import com.exemplo.chatplus.service.ChatDelayService;
@@ -52,8 +51,6 @@ public final class ChatPlus extends JavaPlugin {
         getServer().getPluginManager().registerEvents(chatColorGui, this);
         getServer().getPluginManager().registerEvents(
                 new ChatListener(this, configManager, chatService, chatDelayService), this);
-        getServer().getPluginManager().registerEvents(
-                new EntradaSaidaListener(configManager), this);
         getServer().getPluginManager().registerEvents(new UnknownCommandListener(configManager), this);
 
         getLogger().info("ChatPlus habilitado com sucesso.");
